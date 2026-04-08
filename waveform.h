7bae4323d1556414cd32d5e54da5d85c0899d911
Winston-C4s <yu3.lin@live.uwe.ac.uk>
2026-04-08 05:54:13 +0100

    Update waveform.h
    Add WaveformSample structure definition in waveform.h

--- waveform.h
+++ waveform.h
@@ -1,1 +1,17 @@
+#ifndef WAVEFORM_H
+#define WAVEFORM_H
 
+#include <stddef.h>
+
+typedef struct {
+    double time;
+    double voltageA;
+    double voltageB;
+    double voltageC;
+    double current;
+    double frequency;
+    double powerFactor;
+    double thd;
+} WaveformSample;
+
+#endif
