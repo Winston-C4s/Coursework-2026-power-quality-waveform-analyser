6b667704ccedc073dd930da21871c7f23fab669d
Winston-C4s <yu3.lin@live.uwe.ac.uk>
2026-04-09 22:16:48 +0100

    Update waveform.c
    adds the first real waveform analysis function
    -It calculates the RMS voltage for phase A using the waveform samples loaded from the CSV file

--- waveform.c
+++ waveform.c
@@ -1,1 +1,12 @@
+#include <math.h>
 #include "waveform.h"
+
+double calculate_rms_A(WaveformSample *samples, size_t count) {
+    double sum_sq = 0.0;
+
+    for (size_t i = 0; i < count; i++) {
+        sum_sq += samples[i].voltageA * samples[i].voltageA;
+    }
+
+    return sqrt(sum_sq / count);
+}
