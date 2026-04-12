c8bba15ce565e096838370c878e4a51ea04b34e4
Winston-C4s <yu3.lin@live.uwe.ac.uk>
2026-04-12 02:47:55 +0100

    Clean up comments and match code with devlog updates
    Updated comments and section for io,h
    - change comments with Updates 1–5 in the development log
    - makes the code easier to explain

--- io.h
+++ io.h
@@ -4,6 +4,9 @@
 #include <stddef.h>
 #include "waveform.h"
 
+
+// ----------- Update-3: CSV File Loading(declare the function used to load waveform data from a CSV file)
+
 WaveformSample *load_csv_data(const char *filename, size_t *count);
 
 #endif
