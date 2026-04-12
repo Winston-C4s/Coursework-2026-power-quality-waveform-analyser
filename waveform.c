566ef2e3379f3cdb1373d1e3627575c68e592e03
Winston-C4s <yu3.lin@live.uwe.ac.uk>
2026-04-12 02:48:49 +0100

    Clean up comments and match code with devlog updates
    Updated comments and section for waveform.c
    - change comments with Updates 1–5 in the development log
    - makes the code easier to explain

--- waveform.c
+++ waveform.c
@@ -1,12 +1,21 @@
 #include <math.h>
 #include "waveform.h"
 
+// ----------- Update-5: Phase A RMS Value Calculation
+
 double calculate_rms_A(WaveformSample *samples, size_t count) {
+
+    // -------- Update-5: Store the total of squared voltage values
     double sum_sq = 0.0;
 
+    // -------- Update-5: Go through every waveform sample
     for (size_t i = 0; i < count; i++) {
+
+        // square the phase A voltage and add it to the total
         sum_sq += samples[i].voltageA * samples[i].voltageA;
     }
 
+    // -------- Update-5: Return the RMS value
+    // divide by the number of samples, and then take the square root
     return sqrt(sum_sq / count);
 }
