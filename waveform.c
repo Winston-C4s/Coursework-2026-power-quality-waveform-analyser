9ba8504829e2da8f25c83fca2b8566b31e1991c5
Winston-C4s <yu3.lin@live.uwe.ac.uk>
2026-04-13 01:50:19 +0100

    Add peak-to-peak calculation for phase A
    This update:
    - adds the peak_to_peak_A() function in waveform.c
    - declares the function in waveform.h
    - integrates the calculation into Main.c
    - prints the peak-to-peak result after loading the waveform data
    
    The result is 650.00 V, which matches the expected waveform range

--- waveform.c
+++ waveform.c
@@ -19,3 +19,26 @@ double calculate_rms_A(WaveformSample *samples, size_t count) {
     // divide by the number of samples, and then take the square root
     return sqrt(sum_sq / count);
 }
+double peak_to_peak_A(WaveformSample *samples, size_t count) {
+    if (count == 0) {
+    return 0.0;
+}
+    // -------- Update-6: Store the total of max and min voltage values
+    double max_volt = samples[0].voltageA;
+    double min_volt = samples[0].voltageA;
+
+    // -------- Update-6: Go through every waveform sample
+    for (size_t i = 0; i < count; i++) {
+
+        // update max and min voltage values
+        if (samples[i].voltageA > max_volt) {
+            max_volt = samples[i].voltageA;
+        }
+        if (samples[i].voltageA < min_volt) {
+            min_volt = samples[i].voltageA;
+        }
+    }
+
+    // -------- Update-6: Return the peak-to-peak value
+    return max_volt - min_volt;
+}
