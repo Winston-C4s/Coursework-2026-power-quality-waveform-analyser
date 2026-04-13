d612b8a4a8bad2b71f234c23bf87af2cb1b4eb9a
Winston-C4s <yu3.lin@live.uwe.ac.uk>
2026-04-13 03:07:25 +0100

    Dc offest result calculation
    This update
    - 'waveform.c' – added the DC offset calculation function
    - 'waveform.h' – declared the new function
    - 'Main.c' – called the function and printed the result

--- waveform.c
+++ waveform.c
@@ -3,7 +3,7 @@
 
 // ----------- Update-5: Phase A RMS Value Calculation
 
-double calculate_rms_A(WaveformSample *samples, size_t count) {
+double rms_A(WaveformSample *samples, size_t count) {
 
     // -------- Update-5: Store the total of squared voltage values
     double sum_sq = 0.0;
@@ -41,4 +41,20 @@ double peak_to_peak_A(WaveformSample *samples, size_t count) {
 
     // -------- Update-6: Return the peak-to-peak value
     return max_volt - min_volt;
+}
+double DC_offset_A(WaveformSample *samples, size_t count) {
+
+    // -------- Update-7: dc offset is the average voltage value, so we need to store the total of voltage values
+    double sum = 0.0;
+
+    // -------- Update-7: Go through every waveform sample
+    for (size_t i = 0; i < count; i++) {
+
+        // add the phase A voltage to the total
+        sum += samples[i].voltageA;
+    }
+
+    // -------- Update-7: Return the DC offset value
+    // divide by the number of samples
+    return sum / count;
 }
