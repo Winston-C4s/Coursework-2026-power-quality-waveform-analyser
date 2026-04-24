94f0972d7668708a80717e934a5ba9c2dd4e75c9
Winston-C4s <yu3.lin@live.uwe.ac.uk>
2026-04-24 01:46:46 +0100

    Improve output formatting(update-14)
    This update:
    - adds section titles
    - groups related output together
    - improves the display

--- Main.c
+++ Main.c
@@ -33,27 +33,34 @@ int main(int argc, char *argv[]) {
     double rmsB = rms(samples, sample_count, 'B');
     double rmsC = rms(samples, sample_count, 'C');
     //--------Update-5: Calculate RMS, this is the first analysis result added to the program
+    printf("Analysis Results:\n");
     printf("RMS Voltage A: %.2f V\n", rmsA);
     printf("RMS Voltage B: %.2f V\n", rmsB);
     printf("RMS Voltage C: %.2f V\n", rmsC);
+    printf("----------------------------------------------\n");
     //--------Update-6: Calculate Peak-to-Peak, this is the second analysis result added to the program
     double ptpA = peak_to_peak(samples, sample_count, 'A');
     double ptpB = peak_to_peak(samples, sample_count, 'B');
     double ptpC = peak_to_peak(samples, sample_count, 'C');
+    printf("Peak-to-Peak Voltage Analysis:\n");
     printf("Peak-to-Peak Voltage A: %.2f V\n", ptpA);
     printf("Peak-to-Peak Voltage B: %.2f V\n", ptpB);
     printf("Peak-to-Peak Voltage C: %.2f V\n", ptpC);
+    printf("----------------------------------------------\n");
     //--------Update-7: Calculate DC Offset, this is the third analysis result added to the program
     double dcA = DC_offset(samples, sample_count, 'A');
     double dcB = DC_offset(samples, sample_count, 'B');
     double dcC = DC_offset(samples, sample_count, 'C');
+    printf("DC Offset Voltage Analysis:\n");
     printf("DC Offset Voltage A: %.2f V\n", dcA);
     printf("DC Offset Voltage B: %.2f V\n", dcB);
     printf("DC Offset Voltage C: %.2f V\n", dcC);
+    printf("----------------------------------------------\n");
     //--------Update-8: Clipp ing detection, this is the fourth analysis result added to the program
     int clipping = clipping_detection(samples, sample_count, 'A');
     int clippingB = clipping_detection(samples, sample_count, 'B');
     int clippingC = clipping_detection(samples, sample_count, 'C');
+    printf("Clipping Detection Analysis:\n");
     if (clipping > 0) {
         printf("Clipping count A: %d\n", clipping);
     } else {
@@ -69,10 +76,12 @@ int main(int argc, char *argv[]) {
     } else {
         printf("No clipping detected in Voltage C.\n");
     }
+    printf("----------------------------------------------\n");
     //--------Update-9: Compliance check, this is the fifth analysis result added to the program
     int compliance = compliance_check(samples, sample_count, 'A');
     int complianceB = compliance_check(samples, sample_count, 'B');
     int complianceC = compliance_check(samples, sample_count, 'C');
+    printf("Compliance Check Analysis:\n");
     if (compliance == 1) {
         printf("Voltage A is compliant\n");
     } else {
@@ -88,25 +97,32 @@ int main(int argc, char *argv[]) {
     } else {
         printf("Voltage C is not compliant\n");
     }
+    printf("----------------------------------------------\n");
         //--------Update-10: Mean frequency analysis, this is the sixth analysis result added to the program
     double meanFreq = mean_frequency(samples, sample_count);
     printf("Mean Frequency: %.2f Hz\n", meanFreq);
     //--------Update-11: Mean power factor analysis, this is the seventh analysis result added to the program
     double meanPF = mean_power_factor(samples, sample_count);
+    printf("----------------------------------------------\n");
     printf("Mean Power Factor: %.2f\n", meanPF);
     //--------Update-12: Mean THD analysis, this is the eighth analysis result added to the program
     double meanTHD = mean_THD(samples, sample_count);
+    printf("----------------------------------------------\n");
     printf("Mean THD: %.2f %%\n", meanTHD);
 
     // Update-4: Print the first 10 samples (only for checking the loaded waveform data)
+        printf("----------------------------------------------\n");
+        printf("First 10 Samples:\n");
     for (size_t i = 0; i < 10 && i < sample_count; i++) {
-        printf("Sample %zu\n", i);
-        printf("Time: %f\n", samples[i].time);
-        printf("Voltage A: %f\n", samples[i].voltageA);
-        printf("Voltage B: %f\n", samples[i].voltageB);
-        printf("Voltage C: %f\n", samples[i].voltageC);
-        printf("\n");
+                printf("----------------------------------------------\n");
+                printf("Sample %zu\n", i);
+                printf("Time: %f\n", samples[i].time);
+                printf("Voltage A: %f\n", samples[i].voltageA);
+                printf("Voltage B: %f\n", samples[i].voltageB);
+                printf("Voltage C: %f\n", samples[i].voltageC);
+                printf("\n");
     }
+    printf("----------------------------------------------\n");
     //----Update-4: Free allocated memory, always free memory after finishing the function
     free(samples);
 
